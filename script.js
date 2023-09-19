06a97db1d2ca0cb423277c057f01c9eeacda4c25
John Doe <johndoe@example.com>
2023-09-19 10:13:25 +0200

    border around

--- script.js
+++ script.js
@@ -3,6 +3,9 @@ function setPos(obj, pos) {
     obj.style.left = `${pos[0]}px`
 }
 
+const getWidth = (element) => parseFloat(window.getComputedStyle(element).width)
+const getHeight = (element) => parseFloat(window.getComputedStyle(element).height)
+
 const fishAreaEl = document.querySelector("#fishArea")
 
 
@@ -19,9 +22,7 @@ function createNewFish() {
 
 function moveAllFish() {
     fishArr.forEach(fish => {
-
-        const computedStyle = window.getComputedStyle(fish)
-        const width = parseFloat(computedStyle.width)
+        const width = getWidth(fish)
 
         const left = Math.floor(Math.random() * (window.innerWidth - width))
         fish.style.left = `${left}px`
@@ -42,6 +43,8 @@ const fishMoveInterval = setInterval(moveAllFish, 600)
 const birdEl = document.querySelector("#bird")
 const birdPos = [0, 0]
 const birdSpeed = 5
+const birdWidth = getWidth(birdEl)
+const birdHeight = getHeight(birdEl)
 
 setPos(birdEl, birdPos)
 
@@ -52,10 +55,10 @@ document.onkeyup = (e) => {keys[e.key] = false}
 
 const birdMoveInterval = setInterval(() => {
     
-    if (keys["ArrowUp"]) birdPos[1] -= birdSpeed
-    if (keys["ArrowDown"]) birdPos[1] += birdSpeed
-    if (keys["ArrowLeft"]) birdPos[0] -= birdSpeed
-    if (keys["ArrowRight"]) birdPos[0] += birdSpeed
+    if (keys["ArrowUp"] && birdPos[1] - birdSpeed > 0) birdPos[1] -= birdSpeed
+    if (keys["ArrowDown"] && birdPos[1] + birdSpeed + birdWidth < window.innerWidth) birdPos[1] += birdSpeed
+    if (keys["ArrowLeft"] && birdPos[0] - birdSpeed > 0) birdPos[0] -= birdSpeed
+    if (keys["ArrowRight"] && birdPos[0] + birdSpeed + birdHeight < window.innerHeight) birdPos[0] += birdSpeed
 
     setPos(birdEl, birdPos)
 
